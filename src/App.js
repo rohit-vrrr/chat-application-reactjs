dfad44d13100b4b379bf05ecef47dafa47a3d49a
Rohit <vr.rohit03@gmail.com>
2021-06-30 18:36:48 +0530

    Updated chat components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { ChatEngine } from "react-chat-engine";
 
+import ChatFeed from "./components/ChatFeed";
+
 import "./App.css";
 
 
@@ -7,9 +9,10 @@ const App = () => {
     return (
         <ChatEngine 
             height="100vh"
-            projectID=""
-            userName=""
-            userSecret=""
+            projectID="0f9a17df-651c-4927-8e33-14bb3bef9b94"
+            userName="rohit"
+            userSecret="123123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     );
 }
